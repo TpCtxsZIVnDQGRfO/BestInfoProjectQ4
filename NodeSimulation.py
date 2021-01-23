703aa5b49b3247382331f5bc20e4d2d21c8df946
Bjarne Thal <thal.bjarne@gmail.com>
2021-01-23 20:36:50 +0100

    Krankenhäuser v1
    - eigene Klasse
    - in NodeSimulation
    - Koordinaten, max Anzahl an "Patienten"
    - Patienten haben keine Verbindungen
    - Patienten werden immun
    - nicht alle Nodes gehen zum Krankenhaus
    - Visual: Quadrat (klassisch rotes Kreuz auf weiß?)
    - Verbindung Patienten-Krankenhaus visuell anders
    - Bewegung zu Krankenhaus auf direktem Weg
    - Patienten drapieren sich mit Abstand um Krankenhaus
    - Nodes gehen nur zu Krankenhäusern die nicht zu weit weg sind

--- NodeSimulation.py
+++ NodeSimulation.py
@@ -9,6 +9,7 @@ from PIL import ImageFont
 import cv2
 import glob
 import os
+
 from os.path import isfile, join
 
 import shutil
@@ -39,12 +40,19 @@ class Simulation:
         self.barrierWidth = 2
         self.connecColour = "white"
         self.connecWidth = 1
+        self.KH = [] #Array von Krankenhaus-Objekten
+        self.KHWidth = 50
+        self.KHDistance = math.sqrt(2*(self.KHWidth)**2) #Minimalabstand zwischen zwei KHs
+        self.Behandlungsdauer = 2 #in Sekunden
+        self.KHWeg = 300 #Maxweg, den ein Patient zum KH auf sich nimmt
 
-        
-        shutil.rmtree('pix\\')#leerer Ordner, in dem die einzelnen Frames gespeichert werden
-        newpath = 'pix\\' #Edgecase: löscht momentan noch Inhalt von Ordnern, die schon existieren und so heißen
-        if not os.path.exists(newpath):
+        newpath = 'pix\\'#leerer Ordner, in dem die einzelnen Frames gespeichert werden
+        if not os.path.exists(newpath): #Edgecase: löscht momentan noch Inhalt von Ordnern, die schon existieren und so heißen
             os.makedirs(newpath)
+        else:
+            shutil.rmtree('pix\\')
+            os.makedirs(newpath)
+        
         for i in range(flowchart.shape[0]): #shape ist die Größe (-> [0] Länge in x-Richtung)
             for j in range(flowchart.shape[1]):
                 if flowchart[i,j] != 0:
@@ -65,6 +73,19 @@ class Simulation:
             else:
                 id = self.nodes[-1].ID+1
             self.nodes.append(Knoten(id,k,state,[],None))
+
+    def addRandomKH(self,amount,maxKapa):
+        w = self.KHWidth
+        for i in range(amount):
+            istDaPlatz = False 
+            while istDaPlatz == False:
+                if self.KH == []:
+                    istDaPlatz = True
+                k = [random.randint(w,self.canvas[0]-w),random.randint(w,self.canvas[1]-w)]
+                for kh in self.KH:
+                    if Utility.coordDistance(k,kh.k) >= self.KHDistance:
+                        istDaPlatz = True
+            self.KH.append(KH(k,maxKapa))       
 
     def distance(self,Knoten1, Knoten2): #berechnet den Abstand der beiden Knoten
         return math.sqrt((Knoten1.k[0]-Knoten2.k[0])**2+(Knoten1.k[1]-Knoten2.k[1])**2)
@@ -102,8 +123,32 @@ class Simulation:
         width = self.connecWidth 
         draw = ImageDraw.Draw(image)
         for link in self.links:
-            draw.line((((link[0].k[0]+r)*dis[0], (link[0].k[1]+r)*dis[1]), ((link[1].k[0]+r)*dis[0], (link[1].k[1]+r)*dis[1])), fill=fill,width=width)
+            if link[0].KHState == link[0].KHState == [1,0,0]:
+                draw.line((((link[0].k[0]+r)*dis[0], (link[0].k[1]+r)*dis[1]), ((link[1].k[0]+r)*dis[0], (link[1].k[1]+r)*dis[1])), fill=fill,width=width)
+        for node in self.nodes:
+            if node.KH != None:
+                draw.line((((node.k[0]+r)*dis[0], (node.k[1]+r)*dis[1]), ((node.KH.k[0]+r)*dis[0], (node.KH.k[1]+r)*dis[1])), fill="purple",width=2*width)
         return image
+
+    def visualKH(self, background):
+        r = self.nodeRadius
+        w = round(self.KHWidth/2)
+        symbol = Image.open("hospital.png").resize((self.KHWidth,self.KHWidth))
+        dis = [self.resolution[0]/self.canvas[0],self.resolution[1]/self.canvas[1]] #distortion
+        draw = ImageDraw.Draw(background)
+        #font = ImageFont.truetype("times-ro.ttf", 24)
+        font = ImageFont.truetype("arialbd.ttf", 30)
+        
+        #draw = ImageDraw.Draw(background)
+        for kh in self.KH:
+            k = kh.k
+            background.paste(symbol,(round((k[0]+r)*dis[0]-w), round((k[1]+r)*dis[1]-w)))
+            #draw.text((round((k[0]+r)*dis[0]), round((k[1]+r)*dis[1])),str(kh.belegt),(0,0,0),font=font)
+            text = str(kh.belegt)+"/"+str(kh.kapa)
+            b, h = draw.textsize(text, font=font)
+            h += int(h*0.21)
+            draw.text((int((k[0]+r)*dis[0])-b/2, int((k[1]+r)*dis[1])-h/2), text=text, fill='black', font=font)
+        return background
 
     def visualBarriers(self, background): #malt auf das gegebene Bild die Barrierenlinien
         r = self.nodeRadius
@@ -130,6 +175,8 @@ class Simulation:
         return image
 
     def visualComplete(self): #erstellt das ganze Bild mit allen Teilen
+        if self.KH != []:
+            return self.visualNodes(self.visualBarriers(self.visualKH(self.visualLinks())))
         return self.visualNodes(self.visualBarriers(self.visualLinks()))
         
     def run(self,steps): #die eigentliche Simulation;
@@ -148,7 +195,7 @@ class Simulation:
                 if self.nodes[j].state == [0,1,0]:
                     for k in range(len(self.nodes[j].connec)):
                         if self.nodes[j].connec[k][0].state == [1,0,0]:
-                            if random.random()<self.flow[0][2]:
+                            if random.random()<self.flow[0][2] and self.nodes[j].KHState != [0,0,1] and self.nodes[j].connec[k][0].KHState != [0,0,1]:
                                 tempI.append(self.nodes[j].connec[k][0])
                         if random.random()<self.flow[1][2]:
                             tempR.append(self.nodes[j])
@@ -158,34 +205,90 @@ class Simulation:
                 j.state = [0,0,1]
 
 #############
-            #im folgenden Teil werden die Nodes bewegt
+            #im folgenden Teil werden die Nodes bewegt, wenn sie 
 #############
             if self.movementRadius != 0:
                 for j in range(len(self.nodes)):
                     node = self.nodes[j]
-                    norm = math.sqrt(node.moveVec[0]**2+node.moveVec[1]**2)
-                    node.moveVec = [node.moveVec[0]/norm,node.moveVec[1]/norm] #Bewegungsvektor normieren
-                    if node.beugungsRate == 0:                          #bei Fragen zu diesem ganzen Beugungszeug einfach Bjarne fragen
-                        node.beugungsRate = random.randint(5,20)        #keine Lust, das jetzt zu erklären
-                        node.modVec = [-node.moveVec[1],node.moveVec[0]] #Modifizierungsvektor steht senkrecht auf dem Bewegungsvektor
-                    node.moveVec = [node.moveVec[0]+node.modVec[0]/20*node.beugungsRate,node.moveVec[1]+node.modVec[1]/20*node.beugungsRate]
-                    fuCoords = [node.k[0]+node.moveVec[0]/10*self.speed,node.k[1]+node.moveVec[1]/10*self.speed] #die Koordinaten, an denen die Node als nächtes wäre
-                    wegVersperrt = False
-                    for barrier in self.barriers:
-                        k1 = node.k
-                        k2 = fuCoords
-                        b1 = barrier[0]
-                        b2 = barrier[1]
-                        if Utility.do_they_intersect(k1, k2, b1, b2): #prüfen, ob die geplante Bewegungstrecke mit einer Barriere kollidiert
-                            wegVersperrt = True
-                    nichtZuWeitWeg = False
-                    if self.movementRadius != None:
-                        if Utility.coordDistance(fuCoords,node.startKoord) <= self.movementRadius:
-                            nichtZuWeitWeg = True
-                    if 0 <= fuCoords[0] <= self.canvas[0] and 0 <= fuCoords[1] <= self.canvas[1] and not wegVersperrt and nichtZuWeitWeg: 
-                        node.k = fuCoords #bewegen, wenn der Weg frei ist und nicht aus dem Bild raus führt (sonst stehen bleiben)
-                    node.beugungsRate -= 1
-                    node.moveVec = [node.moveVec[0]+node.modVec[0]/20,node.moveVec[1]+node.modVec[1]/20] #die Krümmung wird geringer
+
+                    #
+                    if node.state == [0,0,1]:
+                        node.KHState == [1,0,0]
+                    
+                    #zu welchem Krankenhaus, falls krank
+                    if node.state == [0,1,0] and node.Idiot == False and node.KHState == [1,0,0]:
+                        #print(node.ID)
+                        KHsMitPlatz = []
+                        KHDistanzen = []
+                        for kh in self.KH:
+                            if kh.belegt < kh.kapa and Utility.coordDistance(node.k,kh.k) <= self.KHWeg:
+                            #if None in kh.patients:
+                                KHsMitPlatz.append(kh)
+                                KHDistanzen.append(Utility.coordDistance(node.k,kh.k))
+                        #print(KHsMitPlatz)
+                        if KHsMitPlatz != []:
+                            for i in range(len(KHsMitPlatz)):
+                                if KHDistanzen[i] == min(KHDistanzen):
+                                    node.KH = KHsMitPlatz[i]
+                                    node.KHState = [0,1,0]
+                                    #platzGefunden = False
+                                    #for platz in range(len(kh.patients)):
+                                    #    if kh.patients[platz] == None and platzGefunden == False:
+                                    #        kh.patients[platz] = node
+                                    KHsMitPlatz[i].belegt += 1
+                                    #        platzGefunden = True
+
+                    #Bewegung zum Krankenhaus
+                    if node.KHState == [0,1,0]:
+                        vec = [node.KH.k[0]-node.k[0],node.KH.k[1]-node.k[1]]
+                        norm = math.sqrt(vec[0]**2+vec[1]**2)
+                        vec = [vec[0]/norm,vec[1]/norm] #Bewegungsvektor normieren
+                        if norm >= math.sqrt(2*(self.KHWidth)**2)+1/10*3*self.speed: #wenn die Node noch weit entfernt ist
+                            node.k = [node.k[0]+vec[0]/10*3*self.speed,node.k[1]+vec[1]/10*3*self.speed]
+                        else: #wenn die Node beim nächsten Schritt im KH stände
+                            vec = [vec[0]*(norm-math.sqrt(2*(self.KHWidth)**2)),vec[1]*(norm-math.sqrt(2*(self.KHWidth)**2))]
+                            node.KHState = [0,0,1]
+                        node.k = [node.k[0]+vec[0],node.k[1]+vec[1]]
+
+                    #Behandlung am Krankenhaus
+                    if node.KHState == [0,0,1]:
+                        if node.genesungsgrad < 1 and node.state != [0,0,1]:
+                            node.genesungsgrad += 1/(self.Behandlungsdauer*self.fps)
+                        else:
+                            node.genesungsgrad = 0
+                            node.state = [0,0,1]
+                            node.KHState = [1,0,0]
+                            #for j in range(len(node.KH.patients)):
+                            #    if node.KH.patients[j] == node:
+                            #        node.KH.patients[j] = None
+                            node.KH.belegt -= 1
+                            node.KH = None
+
+                    #Bewegung für Nicht-Krankenhausgänger
+                    if node.KHState == [1,0,0]:
+                        norm = math.sqrt(node.moveVec[0]**2+node.moveVec[1]**2)
+                        node.moveVec = [node.moveVec[0]/norm,node.moveVec[1]/norm] #Bewegungsvektor normieren
+                        if node.beugungsRate == 0:                          #bei Fragen zu diesem ganzen Beugungszeug einfach Bjarne fragen
+                            node.beugungsRate = random.randint(5,20)        #keine Lust, das jetzt zu erklären
+                            node.modVec = [-node.moveVec[1],node.moveVec[0]] #Modifizierungsvektor steht senkrecht auf dem Bewegungsvektor
+                        node.moveVec = [node.moveVec[0]+node.modVec[0]/20*node.beugungsRate,node.moveVec[1]+node.modVec[1]/20*node.beugungsRate]
+                        fuCoords = [node.k[0]+node.moveVec[0]/10*self.speed,node.k[1]+node.moveVec[1]/10*self.speed] #die Koordinaten, an denen die Node als nächtes wäre
+                        wegVersperrt = False
+                        for barrier in self.barriers:
+                            k1 = node.k
+                            k2 = fuCoords
+                            b1 = barrier[0]
+                            b2 = barrier[1]
+                            if Utility.do_they_intersect(k1, k2, b1, b2): #prüfen, ob die geplante Bewegungstrecke mit einer Barriere kollidiert
+                                wegVersperrt = True
+                        nichtZuWeitWeg = False
+                        if self.movementRadius != None:
+                            if Utility.coordDistance(fuCoords,node.startKoord) <= self.movementRadius:
+                                nichtZuWeitWeg = True
+                        if 0 <= fuCoords[0] <= self.canvas[0] and 0 <= fuCoords[1] <= self.canvas[1] and not wegVersperrt and nichtZuWeitWeg: 
+                            node.k = fuCoords #bewegen, wenn der Weg frei ist und nicht aus dem Bild raus führt (sonst stehen bleiben)
+                        node.beugungsRate -= 1
+                        node.moveVec = [node.moveVec[0]+node.modVec[0]/20,node.moveVec[1]+node.modVec[1]/20] #die Krümmung wird geringer
 
                         
 class Knoten():
@@ -201,6 +304,10 @@ class Knoten():
         self.moveVec = [math.cos(theta),math.sin(theta)] #erster Bewegungsvektor ist Randompunkt auf Einheitskreis
         self.modVec = [0,0] #der Modifizierungsvektor wird in run() bestimmt
         self.beugungsRate = 0
+        self.KH = None #Krankenhaus, an der die Node gerade hängt
+        self.Idiot = False #Idioten gehen nicht zum Krankenhaus, wenn sie krank sind
+        self.KHState = [1,0,0] #gesund, auf dem Weg, am KH
+        self.genesungsgrad = 0 # 1 bedeutet, dass der Patient gesund und immun ist
 
     def distance(self,Knoten): #berechnet den Abstand zu diesem Knoten
         return math.sqrt((self.k[0]-Knoten.k[0])**2+(self.k[1]-Knoten.k[1])**2)
@@ -211,6 +318,14 @@ class Knoten():
             distance = self.distance(Knotenliste[i])
             if distance < max_distance:
                 self.connec.append([Knotenliste[i], distance])
+
+class KH():
+
+    def __init__(self,Koordinaten,maxKapazität):
+        self.k = Koordinaten
+        self.kapa = maxKapazität
+        self.patients = [None] * self.kapa #Node-Array
+        self.belegt = 0
 
 class Utility(): #allerlei nützliche Funktionen
 
@@ -288,10 +403,11 @@ class Utility(): #allerlei nützliche Funktionen
 
 flowchart = np.array([[0,0.05,0],[0,0,0.00025],[0,0,0]])
 sim = Simulation([],flowchart, 5, 10, 6, 90)
-sim.frames = 1000
+sim.frames = 110
 sim.canvas = [1600,900]
 sim.resolution = [1600,900]
 sim.addRandomNode(400,[1,0,0])
+sim.addRandomKH(3,5)
 sim.maxDist = 50
 sim.movementRadius = 500
 
@@ -302,15 +418,16 @@ sim.connecWidth = 1
 
 sim.barriers.append([[800,100],[800,800]])
 
-for i in range(1):
+for i in range(5):
     sim.nodes[i].state = [0,1,0] #die ersten fünf Nodes sind infiziert
 
 #frameArr = []
 #anzeige = tqdm(total=sim.frames,position=0,leave=False)
 for i in range(sim.frames):
-    sim.run(1)
     #frameArr.append(sim.visualComplete())
+    sim.generate_connections()
     sim.visualComplete().save("pix\\"+"0"*(5-len(str(i)))+str(i)+".jpeg", 'JPEG', ppcm=[100,100], quality=100)
+    sim.run(1)
     print(i)
     #anzeige.set_description("Loading...".format(i))
     #anzeige.update(1)
